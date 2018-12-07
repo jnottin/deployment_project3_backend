662c1015c02a6f8cb07eaee804e20291910f65ca
Sam Nottingham <jnottin92@gmail.com>
2018-12-07 09:46:50 -0500

    fixed connection file for backend

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
 } else {
-    mongoose.connect("mongodb://localhost/pickUpApp", { useCreateIndex: true, useNewUrlParser: true });
+    mongoose.connect("mongodb://localhost/project3roomKind", { useCreateIndex: true, useNewUrlParser: true });
 }
 
 mongoose.Promise = Promise;
